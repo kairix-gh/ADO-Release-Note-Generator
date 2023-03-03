63999eb8cf7987a8e422519aeebb6fd109c96b3f
Kairix <kairix@teameligere.com>
2023-03-03 18:29:21 -0500

    Add project files.

---
+++ ADO-Release-Note-Generator/Models/AppConfig.cs
@@ -0,0 +1,88 @@
+﻿namespace ADO_Release_Note_Generator.Models {
+
+    /// <summary>
+    /// Application configuration from appsettings.json
+    /// </summary>
+    public class AppConfig {
+        public string FooterAddress { get; set; } = "";
+        public string FooterHyperlink { get; set; } = "";
+        public string FooterHyperlinkText { get; set; } = "";
+        public string FooterImagePath { get; set; } = "";
+        public string HeaderImagePath { get; set; } = "";
+
+        public ReleaseInfoConfig ReleaseInfo { get; set; } = new ReleaseInfoConfig();
+        public AzureDevOpsConfig AzureDevOps { get; set; } = new AzureDevOpsConfig();
+        public WorkItemConfig WorkItems { get; set; } = new WorkItemConfig();
+    }
+
+    /// <summary>
+    /// Release Information configuration loaded from appsettings.json
+    /// </summary>
+    public class ReleaseInfoConfig {
+        public string Version { get; set; } = string.Empty;
+
+        public string _dateString = string.Empty;
+
+        public string Date {
+            get {
+                return _dateString;
+            }
+            set {
+                if (_dateString != value) _dateString = value;
+
+                if (DateTime.TryParse(value, out DateTime tmpDateTime)) {
+                    DateTime = tmpDateTime;
+                } else {
+                    DateTime = DateTime.Today;
+                }
+            }
+        }
+
+        public DateTime DateTime { get; private set; }
+    }
+
+    /// <summary>
+    /// Azure DevOps configuration loaded from appsettings.json
+    /// </summary>
+    public class AzureDevOpsConfig {
+        public string Token { get; set; } = string.Empty;
+
+        private string _url = "";
+
+        public string Url {
+            get => _url;
+            set {
+                if (_url != value) _url = value;
+
+                if (Uri.TryCreate(_url, UriKind.Absolute, out Uri? tmpUri)) {
+                    Uri = tmpUri;
+                } else {
+                    Uri = null;
+                }
+            }
+        }
+
+        public Uri? Uri { get; private set; }
+    }
+
+    /// <summary>
+    /// Work Item configuration laoded from appsettings.json
+    /// </summary>
+    public class WorkItemConfig {
+        public string FeaturesQuery { get; set; } = string.Empty;
+        public string FixesQuery { get; set; } = string.Empty;
+
+        private string _workItemFields = "";
+
+        public string WorkItemFields {
+            get => _workItemFields;
+            set {
+                if (_workItemFields != value) _workItemFields = value;
+
+                WorkItemFieldArray = _workItemFields.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            }
+        }
+
+        public string[] WorkItemFieldArray { get; private set; } = new string[0];
+    }
+}
---
+++ ADO-Release-Note-Generator/Program.cs
@@ -0,0 +1,160 @@
+﻿using ADO_Release_Note_Generator.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+// Setup Configuration
+var configRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+#if DEBUG
+    .AddUserSecrets("b4a5b96c-9b36-4279-822a-a882526d6965")
+#endif
+    .Build();
+
+AppConfig config = new AppConfig();
+configRoot.Bind(config);
+
+// Get work items from ADO
+Wiql wiql = new Wiql();
+List<WorkItem> bugs = new List<WorkItem>();
+List<WorkItem> stories = new List<WorkItem>();
+
+var credentials = new VssBasicCredential(config.AzureDevOps.Token, string.Empty);
+var connection = new VssConnection(config.AzureDevOps.Uri, credentials);
+var client = connection.GetClient<WorkItemTrackingHttpClient>();
+
+// Get Bug List
+wiql.Query = config.WorkItems.FixesQuery;
+var results = client.QueryByWiqlAsync(wiql).Result;
+var ids = results.WorkItems.Select(i => i.Id).ToArray();
+
+if (ids.Length > 0) {
+    bugs = client.GetWorkItemsAsync(ids, config.WorkItems.WorkItemFieldArray, results.AsOf).Result;
+}
+
+wiql.Query = config.WorkItems.FeaturesQuery;
+results = client.QueryByWiqlAsync(wiql).Result;
+ids = results.WorkItems.Select(i => i.Id).ToArray();
+
+if (ids.Length > 0) {
+    stories = client.GetWorkItemsAsync(ids, config.WorkItems.WorkItemFieldArray, results.AsOf).Result;
+}
+
+Document.Create(container => {
+    // Cover Page
+    container.Page(page => {
+        page.Size(PageSizes.A4);
+        page.Margin(2, Unit.Centimetre);
+        page.PageColor(Colors.White);
+
+        // Content
+        page.Content().AlignMiddle().Column(col => {
+            col.Item().PaddingBottom(10).Text("Release Notes").SemiBold().FontSize(26);
+            col.Item().Text($"{config.ReleaseInfo.DateTime.ToString("MMMM dd, yyyy")} Update");
+        });
+
+        // Footer
+        page.Footer().AlignMiddle().Row(row => {
+            if (File.Exists(Path.Combine(AppContext.BaseDirectory, config.FooterImagePath))) {
+                row.AutoItem().AlignLeft().Height(48).Hyperlink(config.FooterHyperlink).Image(Path.Combine(AppContext.BaseDirectory, config.FooterImagePath), ImageScaling.FitHeight);
+            } else {
+                row.AutoItem().AlignLeft().Width(48).Height(36).PaddingRight(10).Hyperlink(config.FooterHyperlink).Placeholder();
+            }
+
+            row.RelativeItem().AlignMiddle().Column(col => {
+                col.Item().Text(config.FooterAddress);
+                col.Item().Hyperlink(config.FooterHyperlink).Text(config.FooterHyperlinkText);
+            });
+        });
+    });
+
+    container.Page(page => {
+        page.Size(PageSizes.A4);
+        page.Margin(2, Unit.Centimetre);
+        page.PageColor(Colors.White);
+
+        page.Header().AlignMiddle().Row(row => {
+            if (File.Exists(Path.Combine(AppContext.BaseDirectory, config.HeaderImagePath))) {
+                row.AutoItem().AlignLeft().Width(200).Height(36).Image(Path.Combine(AppContext.BaseDirectory, config.HeaderImagePath), ImageScaling.FitHeight);
+            } else {
+                row.AutoItem().AlignLeft().Width(125).Height(36).Placeholder();
+            }
+
+            row.RelativeItem().AlignRight().AlignMiddle().Text($"Release {config.ReleaseInfo.Version} - {config.ReleaseInfo.DateTime.ToString("M/dd/yyyy")}").FontColor(Colors.Grey.Darken2);
+        });
+
+        page.Content().PaddingVertical(1, Unit.Centimetre).Column(x => {
+            string content = string.Empty;
+            int count = 1;
+
+            // Output Features
+            x.Item().Text("Features").FontSize(16);
+            content = string.Empty;
+            foreach (var i in stories) {
+                if (!i.Fields.TryGetValue("Custom.ReleaseNotesNotes", out content)) {
+                    i.Fields.TryGetValue("System.Title", out content);
+                }
+
+                if (content != string.Empty) {
+                    x.Item().PaddingBottom(10).Row(row => {
+                        row.AutoItem().PaddingLeft(10).Text($"{count}. ").FontSize(12);
+                        row.Spacing(10);
+
+                        row.RelativeItem().Column(col => {
+                            col.Item().Text(content).FontSize(12);
+                            col.Item().Text(Placeholders.LoremIpsum()).FontSize(12).FontColor(Colors.Grey.Darken3);
+                        });
+                    });
+                    count++;
+                }
+
+                x.Spacing(10);
+            }
+            x.Item().PageBreak();
+
+            count = 1;
+            x.Item().Text("Fixes").FontSize(18);
+            foreach (var i in bugs) {
+                if (!i.Fields.TryGetValue("Custom.ReleaseNotesNotes", out content)) {
+                    i.Fields.TryGetValue("System.Title", out content);
+                }
+
+                if (content != string.Empty) {
+                    x.Item().PaddingBottom(10).Row(row => {
+                        row.AutoItem().PaddingLeft(10).Text($"{count}. ").FontSize(12);
+                        row.Spacing(10);
+
+                        row.RelativeItem().Column(col => {
+                            col.Item().Text(content).FontSize(12);
+                            col.Item().Text(Placeholders.LoremIpsum()).FontSize(12).FontColor(Colors.Grey.Darken3);
+                        });
+                    });
+                    count++;
+                }
+
+                x.Spacing(10);
+            }
+        });
+
+        page.Footer().Column(col => {
+            col.Item().LineHorizontal(1).LineColor(Colors.Grey.Darken2);
+            col.Spacing(2);
+            col.Item().Row(row => {
+                row.AutoItem().AlignLeft().Text($"Copyright {DateTime.Now.Year}, Transcendent© Corporation").FontColor(Colors.Grey.Darken2); ;
+                row.RelativeItem().AlignRight().Text(x => {
+                    x.Span("Page ").FontColor(Colors.Grey.Darken2); ;
+                    x.CurrentPageNumber().FontColor(Colors.Grey.Darken2); ;
+                    x.Span(" of ").FontColor(Colors.Grey.Darken2); ;
+                    x.TotalPages().FontColor(Colors.Grey.Darken2); ;
+                });
+            });
+        });
+    });
+}).GeneratePdf("demo.pdf");
+
+Console.WriteLine("Done");
