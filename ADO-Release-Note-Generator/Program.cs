d2e6aff373c4debb8159117f46972ddc06f6bc5f
Kairix <kairix@teameligere.com>
2023-03-03 21:29:03 -0500

    - Add basic config validation - Add safety checks when using images - Refactor code

--- ADO-Release-Note-Generator/Program.cs
+++ ADO-Release-Note-Generator/Program.cs
@@ -9,6 +9,9 @@ using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 internal class Program {
+    private static bool useFooterImage = true;
+    private static bool useHeaderImage = true;
+    private static AppConfig Config = new AppConfig();
 
     private static async Task Main(string[] args) {
         // Setup Configuration
@@ -19,34 +22,19 @@ internal class Program {
 #endif
             .Build();
 
-        AppConfig config = new AppConfig();
-        configRoot.Bind(config);
+        configRoot.Bind(Config);
+
+        if (!ValidateConfiguration()) {
+            return;
+        }
 
         // Get work items from ADO
-        Wiql wiql = new Wiql();
         List<WorkItem> bugs = new List<WorkItem>();
         List<WorkItem> stories = new List<WorkItem>();
 
-        var credentials = new VssBasicCredential(config.AzureDevOps.Token, string.Empty);
-        var connection = new VssConnection(config.AzureDevOps.Uri, credentials);
-        var client = connection.GetClient<WorkItemTrackingHttpClient>();
+        (stories, bugs) = await GetAzureDevOpsWorkItems();
 
-        // Get Bug List
-        wiql.Query = config.WorkItems.FixesQuery;
-        var results = client.QueryByWiqlAsync(wiql).Result;
-        var ids = results.WorkItems.Select(i => i.Id).ToArray();
-
-        if (ids.Length > 0) {
-            bugs = client.GetWorkItemsAsync(ids, config.WorkItems.WorkItemFieldArray, results.AsOf).Result;
-        }
-
-        wiql.Query = config.WorkItems.FeaturesQuery;
-        results = client.QueryByWiqlAsync(wiql).Result;
-        ids = results.WorkItems.Select(i => i.Id).ToArray();
-
-        if (ids.Length > 0) {
-            stories = client.GetWorkItemsAsync(ids, config.WorkItems.WorkItemFieldArray, results.AsOf).Result;
-        }
+        string outputFile = $"Transcendent Release {Config.ReleaseInfo.Version} - {Config.ReleaseInfo.DateTime.Year}.{Config.ReleaseInfo.DateTime.Month.ToString().PadLeft(2, '0')}.{Config.ReleaseInfo.DateTime.Day.ToString().PadLeft(2, '0')}.pdf";
 
         Document.Create(container => {
             // Cover Page
@@ -58,50 +46,55 @@ internal class Program {
                 // Content
                 page.Content().AlignMiddle().Column(col => {
                     col.Item().PaddingBottom(10).Text("Release Notes").SemiBold().FontSize(26);
-                    col.Item().Text($"{config.ReleaseInfo.DateTime.ToString("MMMM dd, yyyy")} Update");
+                    col.Item().Text($"{Config.ReleaseInfo.DateTime.ToString("MMMM dd, yyyy")} Update");
                 });
 
                 // Footer
                 page.Footer().AlignMiddle().Row(row => {
-                    if (File.Exists(Path.Combine(AppContext.BaseDirectory, config.FooterImagePath))) {
-                        row.AutoItem().AlignLeft().Height(48).Hyperlink(config.FooterHyperlink).Image(Path.Combine(AppContext.BaseDirectory, config.FooterImagePath), ImageScaling.FitHeight);
+                    if (useFooterImage) {
+                        row.AutoItem().AlignLeft().Height(48).Hyperlink(Config.FooterHyperlink).Image(Path.Combine(AppContext.BaseDirectory, Config.FooterImagePath), ImageScaling.FitHeight);
                     } else {
-                        row.AutoItem().AlignLeft().Width(48).Height(36).PaddingRight(10).Hyperlink(config.FooterHyperlink).Placeholder();
+                        row.AutoItem().AlignLeft().Width(48).Height(36).PaddingRight(10).Hyperlink(Config.FooterHyperlink).Placeholder();
                     }
 
                     row.RelativeItem().AlignMiddle().Column(col => {
-                        col.Item().Text(config.FooterAddress);
-                        col.Item().Hyperlink(config.FooterHyperlink).Text(config.FooterHyperlinkText);
+                        col.Item().Text(Config.FooterAddress);
+                        col.Item().Hyperlink(Config.FooterHyperlink).Text(Config.FooterHyperlinkText);
                     });
                 });
             });
 
+            // Notes Page
             container.Page(page => {
                 page.Size(PageSizes.A4);
                 page.Margin(2, Unit.Centimetre);
                 page.PageColor(Colors.White);
 
+                // Notes Header
                 page.Header().AlignMiddle().Row(row => {
-                    if (File.Exists(Path.Combine(AppContext.BaseDirectory, config.HeaderImagePath))) {
-                        row.AutoItem().AlignLeft().Width(200).Height(36).Image(Path.Combine(AppContext.BaseDirectory, config.HeaderImagePath), ImageScaling.FitHeight);
+                    if (useHeaderImage) {
+                        row.AutoItem().AlignLeft().Width(200).Height(36).Image(Path.Combine(AppContext.BaseDirectory, Config.HeaderImagePath), ImageScaling.FitHeight);
                     } else {
                         row.AutoItem().AlignLeft().Width(125).Height(36).Placeholder();
                     }
 
-                    row.RelativeItem().AlignRight().AlignMiddle().Text($"Release {config.ReleaseInfo.Version} - {config.ReleaseInfo.DateTime.ToString("M/dd/yyyy")}").FontColor(Colors.Grey.Darken2);
+                    row.RelativeItem().AlignRight().AlignMiddle().Text($"Release {Config.ReleaseInfo.Version} - {Config.ReleaseInfo.DateTime.ToString("M/dd/yyyy")}").FontColor(Colors.Grey.Darken2);
                 });
 
+                // Notes Content
                 page.Content().PaddingVertical(1, Unit.Centimetre).Column(x => {
-                    // Output Features & Fixes
+                    // Write Features
                     x.Item().Text("Features").FontSize(16);
                     x.WriteWorkItems(stories);
 
                     x.Item().PageBreak();
 
+                    // Write Fixes
                     x.Item().Text("Fixes").FontSize(18);
                     x.WriteWorkItems(bugs);
                 });
 
+                // Notes Footer
                 page.Footer().Column(col => {
                     col.Item().LineHorizontal(1).LineColor(Colors.Grey.Darken2);
                     col.Spacing(2);
@@ -116,9 +109,86 @@ internal class Program {
                     });
                 });
             });
-        }).GeneratePdf("demo.pdf");
+        }).GeneratePdf(outputFile);
 
         Console.WriteLine("Done");
+    }
+
+    private static async Task<Tuple<List<WorkItem>, List<WorkItem>>> GetAzureDevOpsWorkItems() {
+        Wiql wiql = new Wiql();
+        List<WorkItem> bugs = new List<WorkItem>();
+        List<WorkItem> stories = new List<WorkItem>();
+
+        var credentials = new VssBasicCredential(Config.AzureDevOps.Token, string.Empty);
+        var connection = new VssConnection(Config.AzureDevOps.Uri, credentials);
+        var client = connection.GetClient<WorkItemTrackingHttpClient>();
+
+        // Get Bug List
+        wiql.Query = Config.WorkItems.FixesQuery;
+        var results = await client.QueryByWiqlAsync(wiql);
+        var ids = results.WorkItems.Select(i => i.Id).ToArray();
+
+        if (ids.Length > 0) {
+            bugs = await client.GetWorkItemsAsync(ids, Config.WorkItems.WorkItemFieldArray, results.AsOf);
+        }
+
+        wiql.Query = Config.WorkItems.FeaturesQuery;
+        results = await client.QueryByWiqlAsync(wiql);
+        ids = results.WorkItems.Select(i => i.Id).ToArray();
+
+        if (ids.Length > 0) {
+            stories = await client.GetWorkItemsAsync(ids, Config.WorkItems.WorkItemFieldArray, results.AsOf);
+        }
+
+        return Tuple.Create(stories, bugs);
+    }
+
+    private static bool ValidateConfiguration() {
+        // Check if the cover page footer image exists and is usable
+        if (!File.Exists(Path.Combine(AppContext.BaseDirectory, Config.FooterImagePath))) {
+            Program.useFooterImage = false;
+        }
+
+        // Check if the notes page header image exists and is usable
+        if (!File.Exists(Path.Combine(AppContext.BaseDirectory, Config.HeaderImagePath))) {
+            Program.useHeaderImage = false;
+        }
+
+        // If we don't have a valid ADO Url, we cannot retreive items
+        if (!Uri.IsWellFormedUriString(Config.AzureDevOps.Url, UriKind.Absolute)) {
+            return false;
+        }
+
+        // Check if we have a valid ADO token
+        if (string.IsNullOrWhiteSpace(Config.AzureDevOps.Token.Trim())) {
+            return false;
+        }
+
+        // Confirm we have a query to retreive features
+        if (string.IsNullOrWhiteSpace(Config.WorkItems.FeaturesQuery.Trim())) {
+            return false;
+        }
+
+        // Confirm we have a query to retreive bug fixes
+        if (string.IsNullOrWhiteSpace(Config.WorkItems.FixesQuery.Trim())) {
+            return false;
+        }
+
+        // Confirm we have a fields from the Work Items & that we
+        // always have the required fields.
+        if (Config.WorkItems.WorkItemFieldArray.Length == 0) {
+            return false;
+        } else {
+            if (!Array.Exists(Config.WorkItems.WorkItemFieldArray, e => e.ToLower() == "system.title")) {
+                Config.WorkItems.WorkItemFields += ", System.Title";
+            }
+
+            if (!Array.Exists(Config.WorkItems.WorkItemFieldArray, e => e.ToLower() == "custom.releasenotesnotes")) {
+                Config.WorkItems.WorkItemFields += ", Custom.ReleaseNotesNotes";
+            }
+        }
+
+        return true;
     }
 }
 
@@ -130,7 +200,11 @@ public static class QuestPDFExtensions {
 
         foreach (WorkItem item in items) {
             if (!item.Fields.TryGetValue("Custom.ReleaseNotesNotes", out itemDesc)) {
+#if DEBUG
                 itemDesc = Placeholders.LoremIpsum();
+#else
+                continue;
+#endif
             }
 
             col.Item().PaddingBottom(10).Row(row => {
