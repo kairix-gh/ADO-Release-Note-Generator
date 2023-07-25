5cd92f789f8f8fe5270390fb42e822e7d286c083
Kairix <kairix@teameligere.com>
2023-07-25 11:10:30 -0400

    - Fix title check logic

--- ADO-Release-Note-Generator-Shared/QuestPDF/WorkItemPDFComponent.cs
+++ ADO-Release-Note-Generator-Shared/QuestPDF/WorkItemPDFComponent.cs
@@ -36,11 +36,12 @@ namespace ADO_Release_Note_Generator_Shared.QuestPDF {
                 col.Item().Text(title).FontSize(16);
                 foreach (WorkItem item in items) {
                     // Ensure we have title and description
+                    itemTitle = "";
                     if (!item.Fields.TryGetValue(itemGroup.TitleField, out itemTitle) || string.IsNullOrWhiteSpace(itemTitle)) {
 #if DEBUG
                         itemTitle = $"{itemGroup.Name} Item";
 #else
-                        if (!item.Fields.TryGetValue("System.Title", out itemTitle) || string.IsNullOrWhiteSpace(itemTitle)) {
+                        if (string.IsNullOrWhiteSpace(itemTitle) && !item.Fields.TryGetValue("System.Title", out itemTitle)) {
                             // All items should have a title, if this doesn't we want to skip
                             continue;
                         }
